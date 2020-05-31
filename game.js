1756bc1878a67411af060d8d69aede140b83c9c5
gosteve <nomuojamaro@gmail.com>
2020-06-01 00:17:59 +0100

    created function to track which color a user clicked

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
 var gamePattern = [];
-
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence(){
-  var randomNumber = Math.floor(Math.random()*3);
+  var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
+
+
+$('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var audio = new Audio(`${randomChosenColour}.mp3`);
+audio.play();
 }
+$('.btn').on('click', function() {
+var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  console.log(userClickedPattern);
+});
