cbf06766c9b0e639d631a631c13507f5573e7b95
gosteve <nomuojamaro@gmail.com>
2020-05-31 23:29:53 +0100

    created a game pattern

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-alert('welcome');
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*3);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
