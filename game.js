08b197b8a35ebc27de84bbfd40b8db74a86acadc
gosteve <nomuojamaro@gmail.com>
2020-06-01 02:15:11 +0100

    added key listeners to detect when any key is pressed ans start game

--- game.js
+++ game.js
@@ -1,29 +1,43 @@
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
+var started = false;
+
+$('html').on('keypress',function(){
+  if(!started){
+    $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+}
+
+});
 
 function nextSequence(){
+  level++;
+  $('#level-title').text('level '+level);
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
-
-
 $('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 playSound(randomChosenColour);
 
 }
+
+
 $('.btn').on('click', function() {
 var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  console.log(userClickedPattern);
+  
 });
 
 function playSound(name){
   var audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
 }
+
 
 function animatePress(currentColor){
   $('#'+currentColor).addClass('pressed');
