c64c11fa7a2902bdd2bdd5ced0c50a160bb3db76
gosteve <nomuojamaro@gmail.com>
2020-06-02 13:55:17 +0100

    updated from keypressed to onclick to accomodate mpbile devices that dont have keys to press

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var level = 0;
 var started = false;
 
-$('html').on('keypress',function(){
+$('html').on('click',function(){
   if(!started){
     $("#level-title").text("Level " + level);
       nextSequence();
